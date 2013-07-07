ceff4e205bce13d1d75e3ca1d0d0e7dcb3e2ba6d
alkedr <alexander.kedrik@gmail.com>
2013-07-07 23:37:18 +0400

    add class Client

--- client.hpp
+++ client.hpp
@@ -1,1 +1,26 @@
 #include "magic.hpp"
+
+
+class Client : private Game {
+public:
+
+	Client(std::string host, unsigned short port);
+	
+	//const Game & game() const { return game_; }
+
+	const Player & player(PlayerId id) const { return Game::player(id); }
+	const std::vector<Player> & players() const { return Game::players(); }
+
+	const std::vector<std::unique_ptr<Card>> & cards() const { return Game::cards(); }
+
+	const Turn & turn() const { return Game::turn(); }
+
+	// actions of player
+	void untapCard();
+	void playCardFromHand(CardInGameId cardInGameId);
+	void activateAbility(CardInGameId cardInGameId);
+	void pass();
+
+	virtual void onPriority() {}
+};
+
